975e91038ee8e6c1a17f1ab27ac214b0eab16598
Stefan Moluf <smoluf@ups.edu>
2009-02-18 16:43:23 -0800

    Tweaked admin interface for usability

--- admin.py
+++ admin.py
@@ -7,28 +7,39 @@ admin.site.register(Election, ElectionAdmin)
 
 class RaceAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug' : ('name',)}
+    list_display = ('name', 'slug', 'election', 'rank',)
+    list_filter = ('election',)
+    list_select_related = True
 admin.site.register(Race, RaceAdmin)
 
 try:
     from photologue.models import ImageModel
     class BallotCandidatePhotoAdmin(admin.ModelAdmin):
-        list_display = ['image', 'date_taken', 'admin_thumbnail']
+        list_display = ('candidate', 'image', 'date_taken', 'admin_thumbnail')
     admin.site.register(BallotCandidatePhoto, BallotCandidatePhotoAdmin)
 except ImportError:
     pass
 
 class BallotCandidateAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('first_name', 'last_name', 'race')
+    list_filter = ('race',)
+    list_select_related = True
 admin.site.register(BallotCandidate, BallotCandidateAdmin)
 
 class WriteInCandidateAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'race')
+    list_filter = ('race',)
+    list_select_related = True
 admin.site.register(WriteInCandidate, WriteInCandidateAdmin)
 
 class ElectionAuthorityAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('user', 'election', 'approved',)
+    list_filter = ('election',)
+    list_select_related = True
 admin.site.register(ElectionAuthority, ElectionAuthorityAdmin)
 
 class SealedVoteAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('user_id', 'poll',)
+    list_filter = ('poll',)
+    list_select_related = True
 admin.site.register(SealedVote, SealedVoteAdmin)
